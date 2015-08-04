130110715a7a2d76344fa6471d1e05799f0f672e
Eli White <github@eli-white.com>
2015-08-04 09:20:01 -0700

    Adding a sample for confirming function keys aren't leaked with module.exports

--- samples/issue22/sample.js
+++ samples/issue22/sample.js
@@ -1,13 +1,20 @@
 'use strict'
 
 import SimpleObject from './src/SimpleObject';
+import ModuleExports from './src/ModuleExports';
 import expect from 'expect.js';
 
 describe('Function keys', function(){
 
-  it('should not be leaked', function(){
+  it('should not be leaked with default export', function(){
     let keys = Object.keys(SimpleObject);
     expect(keys).to.eql(['foo', 'bar']);
     expect(typeof (SimpleObject.__Rewire__)).to.be('function');
+  });
+
+  it('should not be leaked with module.exports', function(){
+    let keys = Object.keys(ModuleExports);
+    expect(keys).to.eql(['foo', 'bar']);
+    expect(typeof (ModuleExports.__Rewire__)).to.be('function');
   });
 });
