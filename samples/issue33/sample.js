4ab68ecaccda3e1c0114f042637946ce068a8aa7
Eli White <github@eli-white.com>
2015-08-08 10:52:50 -0700

    Adding a test for function expressions

--- samples/issue33/sample.js
+++ samples/issue33/sample.js
@@ -2,13 +2,23 @@ import Functions from './src/Functions.js';
 import expect from 'expect.js';
 
 describe('Test for issue 33', function() {
-  it('should be able to rewire functions', function() {
-    expect(Functions.testFunc()).to.equal(2);
+  it('should be able to rewire function declarations', function() {
+    expect(Functions.testMyFunc()).to.equal(2);
 
-    Functions.__set__(myFunc, function() {
+    Functions.__set__('myFunc', function() {
       return 3;
     });
 
-    expect(Functions.testFunc()).to.equal(3);
+    expect(Functions.testMyFunc()).to.equal(3);
+  });
+
+  it('should be able to rewire function expressions', function() {
+    expect(Functions.testSecondFunc()).to.equal(2);
+
+    Functions.__set__('mySecondFunc', function() {
+      return 3;
+    });
+
+    expect(Functions.testSecondFunc()).to.equal(3);
   });
 });
--- samples/issue33/src/Functions.js
+++ samples/issue33/src/Functions.js
@@ -2,9 +2,18 @@ function myFunc() {
   return 2;
 };
 
+
+var mySecondFunc = function() {
+  return 2;
+};
+
 var Functions = {
-  testFunc: function() {
+  testMyFunc: function() {
     return myFunc();
+  },
+
+  testSecondFunc: function() {
+    return mySecondFunc();
   }
 };
 
