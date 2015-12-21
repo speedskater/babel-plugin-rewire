117960ef0a8df68c9fb574e666afd87d301058a2
Robert Binna <r.binna@synedra.com>
2015-12-21 15:47:11 +0100

    fixes bulid for different node versions

--- samples/issue82/sample.js
+++ samples/issue82/sample.js
@@ -1,33 +1,19 @@
 import React from 'react';
 
-import ReactDom from 'react-dom'
+import ReactDOMServer from 'react-dom/server'
 import ComponentToTest from './src/ComponentToTest.js';
 import TestUtils from 'react/lib/ReactTestUtils'
 import expect from 'expect.js';
-import jsdom from 'jsdom';
 
 describe('issue82', function() {
-
-	beforeEach(function() {
-		global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
-		global.window = document.parentWindow;
-	});
-
-	afterEach(function() {
-		delete global.window;
-		delete global.document;
-	});
 
 	it('Should be able to rewire React components.', function() {
 		ComponentToTest.__set__('ChildComponent', function RewiredChildComponent(props) {
 			return (<span>Hello from Rewired Component</span>);
-		})
+		});
 
+		var result = ReactDOMServer.renderToStaticMarkup(<ComponentToTest />);
 
-
-		var componenToTest = TestUtils.renderIntoDocument(<ComponentToTest />);
-		var child = TestUtils.findRenderedDOMComponentWithClass(componenToTest, 'content');
-
-		expect(child.textContent).to.be('Hello from Rewired Component');
+		expect(result).to.be('<div class="content"><span>Hello from Rewired Component</span></div>');
 	});
 });
