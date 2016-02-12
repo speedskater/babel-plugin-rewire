6c0bd152b4c322c9e50cf3a65b2adeb73d48262a
Spencer Liechty <spencerliechty@gmail.com>
2016-02-12 13:54:04 -0800

    Add failing usage test

--- samples/namedWildcardExport/sample.js
+++ samples/namedWildcardExport/sample.js
@@ -1,10 +1,10 @@
-import * as wildcardImport from './src/wildcardExport';
+import * as namedWildcardImport from './src/namedWildcardExport';
 import {test1, test2} from './src/namedExports';
 import expect from 'expect.js';
 
-describe('wildcard export of imported object', () => {
+describe('named wildcard export of imported object', () => {
   it('has objects exported from namedExports', () => {
-    expect(wildcardImport.test1).to.equal(test1);
-    expect(wildcardImport.test2).to.equal(test2);
+    expect(namedWildcardImport.namedWildcardExport.test1).to.equal(test1);
+    expect(namedWildcardImport.namedWildcardExport.test2).to.equal(test2);
   });
 });
