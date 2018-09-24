58d54e8afaa9e927c95c8b927de662639938db02
Ross Warren <rosswarren4@gmail.com>
2018-09-24 14:09:42 +0100

    Don't rewire TSTypeReference

--- src/babel-plugin-rewire.js
+++ src/babel-plugin-rewire.js
@@ -31,7 +31,12 @@ module.exports = function({ types: t, template }) {
 		!(parent.type === 'ExportSpecifier') &&
 		!(parent.type === 'ImportSpecifier') &&
 		!(parent.type === 'ObjectTypeProperty') &&
-		!(parent.type === 'ClassMethod')
+		!(parent.type === 'ClassMethod') &&
+		!(parent.type === 'TSDeclareFunction') &&
+		!(parent.type === 'TSExpressionWithTypeArguments') &&
+		!(parent.type === 'TSQualifiedName') &&
+		!(parent.type === 'TSTypeQuery') &&
+		!(parent.type === 'TSTypeReference')
 	}
 
 	function doesIdentifierRepresentAValidReference(path, variableBinding, rewireInformation) {
