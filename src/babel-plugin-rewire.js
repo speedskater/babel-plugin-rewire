cd993d56082baee9627a715ffcd0646606cd9fe9
Bubnov Artem <bf-man@mail.ru>
2023-07-04 12:21:00 +0300

    Add support of optional chaining

--- src/babel-plugin-rewire.js
+++ src/babel-plugin-rewire.js
@@ -31,7 +31,9 @@ module.exports = function({ types: t, template }) {
 		!(parent.type === 'ExportSpecifier') &&
 		!(parent.type === 'ImportSpecifier') &&
 		!(parent.type === 'ObjectTypeProperty') &&
-		!(parent.type === 'ClassMethod')
+		!(parent.type === 'ClassMethod') &&
+		!(parent.type === 'OptionalMemberExpression') &&
+    !(parent.type === 'FunctionDeclaration');
 	}
 
 	function doesIdentifierRepresentAValidReference(path, variableBinding, rewireInformation) {
